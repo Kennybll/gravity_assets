28fc00937d3b0e01f075aeba55927b6e9eb4828b
kennybll <kennybll@outlook.com>
2018-05-27 21:47:19 -0400

    Init

---
+++ gravityjs/examples/chainStore.js
@@ -0,0 +1,16 @@
+import {Apis} from "bitsharesjs-ws";
+import {ChainStore} from "../lib";
+
+Apis.instance("wss://23.92.208.22:8090", true).init_promise.then(res => {
+    console.log("connected to:", res[0].network);
+    ChainStore.init(false).then(() => {
+        ChainStore.subscribe(updateState);
+    });
+});
+
+let dynamicGlobal = null;
+function updateState(object) {
+    // dynamicGlobal = ChainStore.getObject("2.1.0");
+
+    console.log(object);
+}
